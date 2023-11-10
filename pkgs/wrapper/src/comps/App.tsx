35fdcd6998912ac5bbf09c54a7dd54ca66dc4f79
Rizky <rizky05@gmail.com>
2023-11-10 13:55:28 +0700

    fix

--- pkgs/wrapper/src/comps/App.tsx
+++ pkgs/wrapper/src/comps/App.tsx
@@ -9,6 +9,7 @@ export default () => {
   const local = useLocal({
     ref: null as null | HTMLIFrameElement,
     loading: true,
+    reload: false,
   });
 
   useEffect(() => {
@@ -30,105 +31,115 @@ export default () => {
         `
       )}
     >
-      <iframe
-        onLoad={(e) => {
-          const el = e.currentTarget;
-          if (el) {
-            local.ref = el;
-            const win = el.contentWindow;
+      {!local.reload && (
+        <iframe
+          onError={() => {
+            local.reload = true;
             local.render();
+            setTimeout(() => {
+              local.reload = false;
+              local.render();
+            }, 1000);
+          }}
+          onLoad={(e) => {
+            const el = e.currentTarget;
+            if (el) {
+              local.ref = el;
+              const win = el.contentWindow;
+              local.render();
 
-            if (win) {
-              win.postMessage({ mobile: true }, "*");
-            }
-            window.addEventListener(
-              "message",
-              ({ data: raw, currentTarget }) => {
-                console.log(raw);
-                if (typeof raw === "object" && win) {
-                  const msg = raw as unknown as { mobile: true } & {
-                    type: "ready";
-                  };
+              if (win) {
+                win.postMessage({ mobile: true }, "*");
+              }
+              window.addEventListener(
+                "message",
+                ({ data: raw, currentTarget }) => {
+                  console.log(raw);
+                  if (typeof raw === "object" && win) {
+                    const msg = raw as unknown as { mobile: true } & {
+                      type: "ready";
+                    };
 
-                  if (msg.type === "ready") {
-                    if (local.loading) {
-                      local.loading = false;
-                      local.render();
+                    if (msg.type === "ready") {
+                      if (local.loading) {
+                        local.loading = false;
+                        local.render();
 
-                      if (notif && typeof notif.loaded === "function") {
-                        notif.loaded((data: any) => {
-                          win.postMessage({ mobile: true, ...data }, "*");
-                        });
+                        if (notif && typeof notif.loaded === "function") {
+                          notif.loaded((data: any) => {
+                            win.postMessage({ mobile: true, ...data }, "*");
+                          });
 
-                        notif.onReceive = (data) => {
-                          localStorage[`notif-` + data.id] =
-                            JSON.stringify(data);
-                          win.postMessage(
-                            {
-                              mobile: true,
-                              type: "notification-receive",
-                              notif: data,
-                            },
-                            "*"
-                          );
-                        };
-
-                        notif.onTap = (data) => {
-                          if (data) {
-                            try {
-                              const msg =
-                                localStorage[`notif-` + data.notification.id];
-                              if (msg) {
-                                win.postMessage(
-                                  {
-                                    mobile: true,
-                                    type: "notification-tap",
-                                    notif: JSON.parse(msg),
-                                  },
-                                  "*"
-                                );
-                                localStorage.removeItem(
-                                  `notif-` + data.notification.id
-                                );
-                              } else {
-                                win.postMessage(
-                                  {
-                                    mobile: true,
-                                    type: "notification-tap",
-                                    notif: null,
-                                  },
-                                  "*"
-                                );
-                              }
-                            } catch (e) {}
-                          } else {
+                          notif.onReceive = (data) => {
+                            localStorage[`notif-` + data.id] =
+                              JSON.stringify(data);
                             win.postMessage(
                               {
                                 mobile: true,
-                                type: "notification-tap",
-                                notif: null,
+                                type: "notification-receive",
+                                notif: data,
                               },
                               "*"
                             );
-                          }
-                        };
+                          };
+
+                          notif.onTap = (data) => {
+                            if (data) {
+                              try {
+                                const msg =
+                                  localStorage[`notif-` + data.notification.id];
+                                if (msg) {
+                                  win.postMessage(
+                                    {
+                                      mobile: true,
+                                      type: "notification-tap",
+                                      notif: JSON.parse(msg),
+                                    },
+                                    "*"
+                                  );
+                                  localStorage.removeItem(
+                                    `notif-` + data.notification.id
+                                  );
+                                } else {
+                                  win.postMessage(
+                                    {
+                                      mobile: true,
+                                      type: "notification-tap",
+                                      notif: null,
+                                    },
+                                    "*"
+                                  );
+                                }
+                              } catch (e) {}
+                            } else {
+                              win.postMessage(
+                                {
+                                  mobile: true,
+                                  type: "notification-tap",
+                                  notif: null,
+                                },
+                                "*"
+                              );
+                            }
+                          };
+                        }
                       }
                     }
                   }
                 }
-              }
-            );
-          }
-        }}
-        src={config.url}
-        className={cx(
-          css`
-            display: flex;
-            width: 100%;
-            height: 100%;
-          `
-        )}
-      />
+              );
+            }
+          }}
+          src={config.url}
+          className={cx(
+            css`
+              display: flex;
+              width: 100%;
+              height: 100%;
+            `
+          )}
+        />
+      )}
       {local.loading && <Loading />}
     </div>
   );
